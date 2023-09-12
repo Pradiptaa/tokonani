fc4e07680b338476f486a8f913efb8d078c44b99
Pradiptaa <diptaarya18@gmail.com>
2023-09-12 21:04:13 +0700

    commit changes

--- main/views.py
+++ main/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'App Name': 'tokonani',
-        'Name': 'Pradipta Arya Pramudita',
-        'Class': 'PBP F'
+        'name': 'Pradipta Arya Pramudita',
+        'class': 'PBP F'
     }
 
     return render(request, "main.html", context)
