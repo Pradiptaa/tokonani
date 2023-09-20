fb8e6ba446ec835fb195c29e07a9af298cc58bf9
Pradiptaa <diptaarya18@gmail.com>
2023-09-20 11:27:39 +0700

    adding date_added

--- main/models.py
+++ main/models.py
@@ -5,3 +5,4 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
